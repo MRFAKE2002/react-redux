db7641e01c805e7a6c2ab3ba9d51bed795b3fb13
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-09-30 12:04:26 +0330

    Add components in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// components
+import DownBox1 from './components/downBox/DownBox1';
+import UpBox1 from './components/upBox/UpBox1';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <UpBox1 />
+
+      <br/>
+
+    <DownBox1 />
+
+    </div>
+  );
+}
+
+export default App;
