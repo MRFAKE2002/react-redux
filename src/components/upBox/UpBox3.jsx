f33ac1d296f91ff43de6962361f52d16257e28b3
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-01 10:36:25 +0330

    Add counter slice into UpBox component to show its value

--- src/components/upBox/UpBox3.jsx
+++ src/components/upBox/UpBox3.jsx
@@ -1,9 +1,28 @@
+import { useSelector } from "react-redux";
+
 function UpBox3(params) {
+
+  /*
+    Dar inja baraye inke ma betunim az redux va data darun state estefade komim bayad az 'useSelector' ke male react redux
+    estefade konim va dar vorudi tamame data 'store' ke reducer va slice ha dakhelesh hast ro be ma mide va ma dar khoruji
+    migim meghdar value oun slice ke mikhaim ro behemun bede.
+    intori meghdar value state oun slicer dakhel moteghayer gharar migire va mitunim namayesh bedim
+  */
+
+  const counter = useSelector(state => state.counter.value)
+  /* 
+    console.log(counter);
+    '
+    counter: {value: 0}
+    '
+  */
+
+
   return (
     <>
       <div style={{backgroundColor:'white', padding:'30px'}}>
         <h3>UpBox3</h3>
-        <h1>counter:0</h1>
+        <h1>counter:{counter}</h1>
       </div>
     </>
   );
