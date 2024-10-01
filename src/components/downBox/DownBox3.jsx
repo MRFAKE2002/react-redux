65f3dc9f3a629bdeeae268da8202d87bb639e087
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-01 11:31:20 +0330

    Add counterSlicer into DownBox component and Add decrement action when click on button

--- src/components/downBox/DownBox3.jsx
+++ src/components/downBox/DownBox3.jsx
@@ -1,9 +1,42 @@
+// libraries
+import { useDispatch, useSelector } from "react-redux";
+
+// components
+import { counterSlicer } from "../../app/counterSlicer";
+
 function DownBox3(params) {
+    /*
+    Dar inja baraye inke ma betunim az redux va data darun state estefade komim bayad az 'useSelector' ke male react redux
+    estefade konim va dar vorudi tamame data 'store' ke reducer va slice ha dakhelesh hast ro be ma mide va ma dar khoruji
+    migim meghdar value oun slice ke mikhaim ro behemun bede.
+    intori meghdar value state oun slicer dakhel moteghayer gharar migire va mitunim namayesh bedim
+  */
+  const counter = useSelector((state) => state.counter.value);
+  /* 
+      console.log(counter);
+      '
+      counter: {value: 0}
+      '
+    */
+
+  // az 'dispatch' baraye seda zadan oun 'action' ke male ye slice khas az store redux hast estefade mikonim.
+  const dispatch = useDispatch();
+
   return (
     <>
-      <div style={{backgroundColor:'white', padding:'30px'}}>
+      <div style={{ backgroundColor: "white", padding: "30px" }}>
         <h3>DownBox3</h3>
-        <h1>counter:0</h1>
+
+        {/* 
+          ma ye dokme mizarim migim zamani ke rush click shod bia ba estefade az 'dispatch' oun 'action' ke mikhaim ro seda bezan.
+        */}
+        <button onClick={() => dispatch(counterSlicer.actions.decrement())}>
+          decrement
+        </button>
+
+        <br />
+
+        <h1>counter:{counter}</h1>
       </div>
     </>
   );
